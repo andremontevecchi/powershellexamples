e3a52b5ac19c88849518469c7bac290b23240626
Andre Montevecchi <andremon@microsoft.com>
2021-10-25 17:11:36 +0100

    Adding SetContentTypeToHiddenPnP.ps1

---
+++ SetContentTypeToHiddenPnP.ps1
@@ -0,0 +1,19 @@
+﻿#Installing the PnP PowerShell 
+#Install-Module -Name SharePointPnPPowerShellOnline
+$SiteURL = "https://yourtenant.sharepoint.com/sites/yoursite"
+$ListName = "Contacts" # The list name of your list
+$ContentTypeName ="Task" # your content type name that you need to change the visibility
+ 
+#Connect to Pnp Online
+Connect-PnPOnline -Url $SiteURL -ClientId "your client id" -ClientSecret "your client secret"  #-UseWebLogin
+  
+#Get the Context
+$Context = Get-PnPContext
+  
+#Get the content type from List
+$ContentType = Get-PnPContentType -Identity $ContentTypeName -List $ListName
+  
+#Set content type to hidden
+$ContentType.Hidden = $true
+$ContentType.Update($False)
+$Context.ExecuteQuery()
